108dddee6008443a383a470fc8063062fa4a7976
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-05-10 16:38:49 +0200

    Backend - Retrieve 1 or * products.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router();
 const {Product, ValidateProduct} = require('../models/Product');
 
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.json(product);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
 router.post('/', async (req, res) => {
     const validateBody = ValidateProduct(req.body);
 
