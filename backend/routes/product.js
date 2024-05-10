6c49eb56324df4b0cf31fd9a593172c247f81577
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-05-10 16:34:22 +0200

    Backend - Create a single product.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const {Product, ValidateProduct} = require('../models/Product');
 
-router.get('/', async (req, res) => {
-	console.log('get products')
-	res.json('ok')
-})
+router.post('/', async (req, res) => {
+    const validateBody = ValidateProduct(req.body);
+
+    if (validateBody.error) {
+        return res.status(400).send(validateBody.error.details[0].message);
+    }
+
+    let product = await Product.findOne({ name: req.body.name });
+    if (product) {
+        return res.status(400).send('Product already exists');
+    }
+
+    product = new Product({...req.body});
+
+    try {
+        const savedProduct = await product.save();
+        res.json(savedProduct);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
 
 module.exports = router;
