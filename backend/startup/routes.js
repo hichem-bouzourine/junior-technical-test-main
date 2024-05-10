1236fc18ccd42984cc2c5defb320076cd02e0e14
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-05-10 17:03:10 +0200

    Backend - Prefix routes with `/api`.

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -4,6 +4,6 @@ const productRouter = require('../routes/product');
 module.exports = function (app) {
     app.use(express.json()); // Middleware to parse incoming requests with JSON payloads
 
-    app.use('/products', productRouter); // Use the product router for any routes starting with /products
+    app.use('/api/products', productRouter); // Use the product router for any routes starting with /products
 
 }
