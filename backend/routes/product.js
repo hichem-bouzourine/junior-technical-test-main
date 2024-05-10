86b4e614117fa2b88c60c748c24de696d7392dbc
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-05-10 16:39:19 +0200

    Backend - Add PUT route to modify a single product.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {Product, ValidateProduct} = require('../models/Product');
 
+// GET : Get all products
 router.get('/', async (req, res) => {
     try {
         const products = await Product.find();
@@ -11,6 +12,7 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET : Get a specific product
 router.get('/:id', async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
@@ -20,6 +22,7 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// POST : Add a new product
 router.post('/', async (req, res) => {
     const validateBody = ValidateProduct(req.body);
 
@@ -37,6 +40,16 @@ router.post('/', async (req, res) => {
     try {
         const savedProduct = await product.save();
         res.json(savedProduct);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// PUT : Modify a product
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedProduct);
     } catch (err) {
         res.json({ message: err });
     }
